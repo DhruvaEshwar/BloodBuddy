b2ad9f2d30f5a6d9d593330a47fe5cb484c6a5cb
DhruvaEshwar <dhruvase06@gmail.com>
2024-12-07 01:43:55 +0000

    sj

--- main.py
+++ main.py
@@ -718,7 +718,7 @@ def render_page():
             donor_history_page()
         elif st.session_state.page == 'forum':
             forum_page()
-        elif page == "Nearby Facilities Locator":
+        elif st.session_state.page == "Nearby Facilities Locator":
             locator_page()
 
 
